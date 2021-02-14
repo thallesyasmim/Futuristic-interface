54918dc2d8e07bd51c8457d10041a00da5a3a8ea
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 20:40:22 -0300

    Framer Motion image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 import Layout from '../components/Layout'
 
 export default function Home() {
@@ -5,7 +7,11 @@ export default function Home() {
     <Layout>
       <h1 className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">Construa interfaces modernas utilizando Next.js, TaillwindCSS e Framer Motion.</h1>
 
-      
+      <div className="grid w-full grid-cols-1 grid-rows-3">
+        <div className="relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer">
+          <motion.img className="w-full h-full" src="/img/nextjs.svg" alt="Next.js" />
+        </div>
+      </div>
     </Layout>
   )
 }
