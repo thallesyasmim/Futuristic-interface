b80f355410c8d7736d01c73d32649b794efec8c9
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 19:58:15 -0300

    Import Header Component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,9 @@
+import Header from './Header'
+
 const Layout: React.FC = ({ children }) => {
     return (
-        <div>
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
+            <Header />
             {children}
         </div>
     )
