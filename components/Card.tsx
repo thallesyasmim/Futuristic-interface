d9a0c48cd845b3ec28a6dadbce43b57d81d4acf6
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 20:55:31 -0300

    Remove margin-top

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,7 +11,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ href, imgSrc, layoutId }) => { /* LayoutID - It must be the same between two pages to be able to animate with Framer Motion */ 
     return (
         <Link href={href}>
-            <div className="relative h-20 md:h-40 bg-gray-200 hover:bg-gray-50 rounded-xl cursor-pointer transition mt-10">
+            <div className="relative h-20 md:h-40 bg-gray-200 hover:bg-gray-50 rounded-xl cursor-pointer transition">
             <motion.img className="w-full h-full" src={imgSrc} alt="Next.js" layoutId={layoutId} />
             </div>
         </Link>
