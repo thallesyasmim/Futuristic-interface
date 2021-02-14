ba4415468c4d9f5908af64159222dadca9e6e865
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 20:31:21 -0300

    Styling the title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
-      <h1>Thalles Gabriel && Yasmim Cristina</h1>
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">Construa interfaces modernas utilizando Next.js, TaillwindCSS e Framer Motion.</h1>
+
+      
     </Layout>
   )
 }
