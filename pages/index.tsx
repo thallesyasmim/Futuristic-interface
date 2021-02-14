fc3212dd13d99830f536d469e78782e2d11a404e
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 20:55:42 -0300

    Add gap in grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <Layout>
       <h1 className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">Construa interfaces modernas utilizando Next.js, TaillwindCSS e Framer Motion.</h1>
 
-      <div className="grid w-full grid-cols-1 grid-rows-3 mt-10 md:mt-24">
+      <div className="grid w-full grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-4 mt-10 md:mt-24">
         <Card imgSrc="/img/nextjs.svg" href="/nextjs" layoutId="nextjs-logo" />
         <Card imgSrc="/img/tailwind.svg" href="/tailwind" layoutId="tailwind-logo" />
         <Card imgSrc="/img/framermotion.svg" href="/framermotion" layoutId="framermotion-logo" />
