f938ae957e0a02b4dcd244f314fabbdb19e30211
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 21:25:47 -0300

    Change Layout Per <div>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import Layout from '../components/Layout'
 import Card from '../components/Card'
 
 export default function Home() {
   return (
-    <Layout>
+    <div>
       <h1 className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">Construa interfaces modernas utilizando Next.js, TaillwindCSS e Framer Motion.</h1>
 
       <div className="grid w-full grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-4 mt-10 md:mt-24">
@@ -12,6 +11,6 @@ export default function Home() {
         <Card imgSrc="/img/framermotion.svg" href="/framermotion" layoutId="framermotion-logo" />
       </div>
       
-    </Layout>
+    </div>
   )
 }
