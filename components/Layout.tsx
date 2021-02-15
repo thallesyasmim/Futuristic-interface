2a9e627e99b377bf4b836470565585158bf5d46e
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 21:23:42 -0300

    Add padding-bottom

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import Header from './Header'
 
 const Layout: React.FC = ({ children }) => {
     return (
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8 sm:pb-6 md:pb-8">
             <Header />
             {children}
         </div>
