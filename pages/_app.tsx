f317aa6f7f4f495217de4268ec002e06aafc0637
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-14 21:24:10 -0300

    Layout in all pages and AnimateSharedLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/tailwind.css'
 import '../styles/globals.css'
 
+import Layout from '../components/Layout'
+// All pages will have the Layout component
+import { AnimateSharedLayout } from 'framer-motion' 
+// Putting this component of Framer Motion we have the animation between one page and another
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnimateSharedLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimateSharedLayout>
+  )
 }
 
 export default MyApp
